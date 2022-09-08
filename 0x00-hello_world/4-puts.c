9d3ffd0372d4d98b3f860ba2cb953ee5325ab3a6
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-09-08 09:48:20 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-
 /**
- * main-Entry point
+ * main - entry point
  *
  * Return: 0 after printing function
  */
 int main(void) 
+
 { 
 	
 	puts("\"Programming is like building a multilingual puzzle");
