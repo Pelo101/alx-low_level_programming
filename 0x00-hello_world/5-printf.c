481bda99fc20b72c3b7b4c6deb49fb7b101dda49
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-09-08 11:21:14 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio>
+
+/**
+ * main -entry point 
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,/n")
+		Return (0);
+}
