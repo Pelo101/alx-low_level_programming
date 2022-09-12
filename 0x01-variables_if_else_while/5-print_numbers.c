89049ec3e2dc010c3d4e7d89d06b0378e3ed9ed4
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-09-12 10:40:58 -0700

    5 print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (succesful)
+ */
+
+int main(void)
+
+{
+	char n;
+
+	for (n = '0'; n  <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar{'\n')
+
+	return (0);
+
+}
