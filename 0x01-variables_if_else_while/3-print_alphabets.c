66aa241d645f25e5a68a88157bb51866fddffc0d
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-09-12 08:10:49 -0700

    print

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ *
+ * Return: Always 0 (succcess)
+ */
+int main (void)
+
+{
+
+
+	char c;
+
+	for (c ='a'; c < = 'z'; c++)
+	{
+
+		putchar(c);
+
+	}
+
+	for (c = 'A'; c < = 'Z'; C++)
+	{
+		putchar(c);
+
+	}
+		putchar('\n');
+
+		return (0);
+
+}
