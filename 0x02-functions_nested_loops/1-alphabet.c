06fbc90e01a04871d44f95980d9ea732f0ced839
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-09-13 08:01:22 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function to print abc
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet(void);
+{
+
+	char (c);
+
+	for (c = 'a'; c  <=  'z'; c++)
+
+	{
+		_putchar(c);
+
+	}
+
+	_puctchar('\n');
+
+
+
+
+}
