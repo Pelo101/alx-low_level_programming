38c2c738bfe1e43aa05f5be7f6a6c96b2af74a7c
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-09-15 05:15:04 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add to integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: integer (successful)
+ */
+
+int add(int a, int b)
+{
+	return (a+b);
+}
