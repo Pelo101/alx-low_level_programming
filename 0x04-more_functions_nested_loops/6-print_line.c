d4bf128976dcfdd6efc4fb2f6e5768fffacfc464
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-09-16 13:47:47 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line  - prints a straight line
+ * @n:parameter
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	while (n-- >  0 )
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+
+}
+
+
