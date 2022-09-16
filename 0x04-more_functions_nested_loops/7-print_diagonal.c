e8478d5ee6b5c68501ca7b41e51cb03178e6668d
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-09-16 14:19:29 -0700

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * prints_diagonal - prints a diagonal
+ * @n: parameter
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int c, i;
+
+	if (n  >  0)
+	{
+
+		for  (c =  0;  c  <  n;  c++)
+		{
+
+			for (i  = 0;  i  <  n;  n++)
+			{
+				_putchar(' ');
+
+
+
+			}
+			_putchar('\\');
+
+		}
+		if  (c  == (n - 1))
+		{
+			continue;
+
+		}
+		_putchar('\n');
+
+	}
+	_putchar('\n')
+}
