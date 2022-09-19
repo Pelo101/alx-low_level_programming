69d9f6efd401c6e51b4cf53c7a8c87d93fdaa455
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-09-19 13:08:35 -0700

    copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,28 @@
+# include "main.h"
+
+/**
+ * _strcpy: copies the content of one string to another
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: *dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+	int i;
+
+	int i  =  0
+
+
+	while (*(src  +  i)  !=   '\0')
+	{
+		*(dest + i)  =  *(src  + i);
+		i++;
+	}
+	*(dest  + i )  = '\0';
+
+
+	return (dest);
+}
