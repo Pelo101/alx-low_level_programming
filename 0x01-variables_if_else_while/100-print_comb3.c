efe2cb6430eaff6a623c75f0bafc0d25775e36fb
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-09-22 15:53:16 -0700

    leet

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; j <= '9'; j++)
+
+	{  
+		for (j = '1'; j <= '9'; j++)
+		
+		{
+			if (j > i) 
+
+			{
+				putchar(i+'0')
+				putchar(j+'0'
+		)
+
+			}
+
+			if (i! = 8)
+			
+			{
+				putchar(',');
+
+				putchar(' ');
+			}
+
+
+
+
+
+
+
+
+
+
+
+		} 
+
+
+	}
+}
