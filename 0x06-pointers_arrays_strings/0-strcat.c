1de7923d1a475f98d8baff7e5883c2885d3b35dc
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-09-22 07:07:14 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+# include "main.h"
+
+/**
+ * _strcat- function that concatenates two strings
+ * @dest: destination file
+ * @src: source file that is appended
+ *
+ * Return: dest (success)
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+	int i, int j;
+
+	for (i = 0; dest[i] !=  '\0'; i++)
+
+	for (j = 0; src[j] != '\0'; j++, i++)
+	{
+		dest[i]	= src[j];
+
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
