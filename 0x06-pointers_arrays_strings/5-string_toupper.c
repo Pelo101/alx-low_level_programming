c3b39e76903d3ee3b80e3aa4099363a256871702
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-09-22 09:02:00 -0700

    uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -14,11 +14,10 @@ char *string_toupper(char *)
 
 	while (str[i] != '\0')
 	{
-		if (str[i]  >=  'a' &&  str[i]  <=  'z')
+		if (str[i]  >=  'a'  &&  str[i]  <=  'z')
 
 			str[i] -= 32;
-
-			i++;
+		i++;
 	}
 	return (str);
 }
