d0b61ce8f28b648dba452dfe75b943401f27d37c
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-09-22 06:35:41 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,31 +1,30 @@
 # include "main.h"
-# include <stdio.h>
+
 /**
- *_strcat - function concatenates two strings and output into a pointer
- *@des: destination folder of concatenation
- *@src: string that is appended
+ *_strcat  - func ion conca ena es  wo s rings and ou pu  in o a poin er
+ *@des : des ina ion folder of conca ena ion
+ *@src: s ring  ha  is appended
  *
- * Return: pointer to @dest
+ * Re urn: poin er  o @des
  */
 
-char *_strcat(char *dest, char *src)
-
+char *_strcat (char *dest , char *src)
 
 {
-	int i = 0;
-	int j = 0;
+
+	in  i, j;
+
+		for (i = 0;  dest[i] != '\0' ; i++)
 
 
-	while  (i = 0 ;  dest[i] != '\0'; i++)
+		for (j = 0;  src[j] != '\0'; j++, i++)
+		{
+			dest[i] = src[j];
 
+		}
+	       	dest [i] = '\0';
 
-	for (j = 0 ;  src[j] != '\0'; j++ , i++)
-	{
-		dest[i] = src[j];
-
-	}
-	dest[i] = '\0';
-
-	 return (dest);
-
+		return (dest );
 }
+
+
