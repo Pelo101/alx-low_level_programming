f1a4172559edeb88bd46c6deb10a164ab3e79e08
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-09-22 08:13:27 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,12 +14,12 @@ int _strcmp(char *s1, char *s2)
 
 	while (1)
 	{
-		if (s1[i] == '\0' && s2[i] == '\0')
+		if (s1[i] == '\0' &&   s2[i] == '\0')
 			break;
 
 		else if (s1[i] == '\0')
 		{
-			diff ==  s2[i];
+			diff =  s2[i];
 			break;
 		}
 		else if (s2[i] == '\0')
