349a040afa5c05778a871ad419e80721a9a9671d
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-09-22 05:39:44 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,7 +15,8 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int j = 0;
 
-	for (i = 0 ;  dest[i] != '\0'; i++)
+
+	while  (i = 0 ;  dest[i] != '\0'; i++)
 
 
 	for (j = 0 ;  src[j] != '\0'; j++ , i++)
