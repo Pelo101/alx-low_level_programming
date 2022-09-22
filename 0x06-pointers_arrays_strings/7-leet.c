ea20416f75c2eaec87450fbffc72f22b5812285a
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-09-22 14:31:27 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,7 +1,7 @@
 # include "main.h"
 
 /**
- * leet  - encodes a string into  1337
+ * leet - encodes a string into  1337
  * @s: string to encode
  *
  * Return: address of s
@@ -17,7 +17,7 @@ char *leet(char *s)
 
 	for (i =  0;  *(s  +  i);  i++)
 	{
-		for (j =  0;  j   <=   9; j++)
+		for (j =  0;  j  <=  9; j++)
 
 		{
 			if  (a[j]   ==   *(s  +  i))
