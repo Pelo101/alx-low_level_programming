9b4c8520214a719909c5372c6a04b9b321c3b244
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-09-22 07:15:26 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 # include "main.h"
+# include <stdio.h>
 
 /**
  * _strcat- function that concatenates two strings
@@ -13,7 +14,7 @@ char *_strcat(char *dest, char *src)
 
 	int i, int j;
 
-	for (i = 0; dest[i] !=  '\0'; i++)
+	for (i = 0; dest[i] != '\0'; i++)
 
 	for (j = 0; src[j] != '\0'; j++, i++)
 	{
