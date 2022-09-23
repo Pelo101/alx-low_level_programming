144e76059e93366ece1ff0416e5341a262cc093e
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-09-22 18:05:43 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,7 +13,7 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int index = 0;
 
-	dest_len = 0;
+      int dest_len = 0;
 
 
 	while (dest[index++])
@@ -22,4 +22,7 @@ char *_strncat(char *dest, char *src, int n)
 	for (index  = 0;  src[index]   && index  < n; index++)
 
 		dest[dest_len++]  =  src[index];
+
+	return (dest);
+
 }
