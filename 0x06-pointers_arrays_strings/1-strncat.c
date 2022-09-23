b003be6aef4744181a5e7dd6df1a4684a172b404
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-09-22 18:00:09 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+# include "main.h"
+
+/**
+ * _strncat - concatenate two strings but add inputed number of bytes
+ * @dest: destination string
+ * @src: string to be appended to dest
+ * @n: integer parameter to compare
+ * Return: new concatenated string
+ */
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0;
+
+	dest_len = 0;
+
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index  = 0;  src[index]   && index  < n; index++)
+
+		dest[dest_len++]  =  src[index];
+}
