ea47af458052bdbb66e10df5e1696225592a88bf
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-09-28 09:36:20 -0700

    length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+# include "main.h"
+
+/**
+ *_strlen_recursion - returns length of string
+ *@s: length of string to be determined
+ *
+ *Return: length of strinh
+ */
+
+int _strlen_recursion(char *s)
+{
+	int sum = 0;
+
+	if (*s !=  '\0')
+	{
+		sum++;
+		sum += _strlen_recursion(s  + 1);
+	}
+}
