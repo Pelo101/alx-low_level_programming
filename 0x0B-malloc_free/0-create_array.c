a2ef8fdce23b5a1124e238a193c6080b11b54e82
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-10-04 10:59:03 -0700

    arr git commit -m array git push git commit -m arr

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,1 +1,29 @@
+# include "main.h"
 
+/**
+ *create_array - creates an array  of chars, initializes to a specific char
+ * @size: size of the array
+ * @c: character to insert
+ * Return: NULL if size is zero or it fails
+ * Pointer to array if everything is normal
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+
+	unsigned int i;
+
+	if  (p == 0)
+		return (NULL);
+	p = malloc(sizeof(char) * size);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i  = 0;   i  < size ; i++)
+		p[i] = c;
+
+	return (p);
+
+}
