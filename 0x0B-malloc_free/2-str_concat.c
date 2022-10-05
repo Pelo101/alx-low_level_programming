0fc74e8efd085b5df44cd1eacdb8a8bb8810dc05
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-10-05 13:15:47 -0700

    comc git commit -m concat git push git commit -am comc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,7 +12,7 @@ char *str_concat(char *s1, char *s2)
 {
 	char *concat_str;
 
-	int i,  concat_index = 0, len = 0;
+	int index,  concat_index = 0, len = 0;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -20,7 +20,7 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i  = 0;  s1[i]  ||  s2[i];  i++)
+	for (index  = 0;  s1[index]  ||  s2[index];  index++)
 		len++;
 
 	concat_str  =  malloc(sizeof(char) * len);
@@ -28,10 +28,9 @@ char *str_concat(char *s1, char *s2)
 	if (concat_str == NULL)
 		return (NULL);
 
-	for (i  = 0;  s1[i];  i++)
-		concat_str[concat_index++] = s1[i];
-	for (i = 0; s2[i]; i++)
-		concat_str[concat_index] = s2[i];
-
+	for (index = 0;  s1[index];  index++)
+		concat_str[concat_index++] = s1[index];
+	for (index = 0; s2[index]; index++)
+		concat_str[concat_index] = s2[index];
 	return (concat_str);
 }
