b454df78b59ac97935ebd281fdd1c5d03a53b0f7
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-10-12 12:35:41 -0700

    name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointer.h"
+
+/**
+ *print_name - prints a name
+ *@name: name of pointer to be printed
+ *@f: function to pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	{
+		f(name);
+	}
+
+}
