4b93c0cb24cee8289207e6562bfdef581aa0feb0
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-10-18 15:53:12 -0700

    strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,7 +17,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list list;
 
 	 va_start(list, n);
-		 for (i = 0; i < n; i++)
+	for (i = 0; i < n, i++)
+
 		{
 		str = va_arg(list, char *);
 		if (str)
