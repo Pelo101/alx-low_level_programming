92350a7a2a33fe0160eacdd3b26903e416699d94
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-10-19 06:51:34 -0700

    dogs

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef _DOG_H_FILE
+#define _DOG_H_FILE
+/**
+*struct dog - structure information about dogs
+*@name: name of dog 
+*@age: age of dog
+*@owner: owner of dog
+*/
+
+typedef struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+}dog_t;
+
+
+int _putchar(char);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
+#endif 
