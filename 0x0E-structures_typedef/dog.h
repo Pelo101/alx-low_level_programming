b14a773e26b3ca2173f0c744eb3f38b81b09ebd9
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-10-19 06:59:33 -0700

    dogs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,17 +2,17 @@
 #define _DOG_H_FILE
 /**
 *struct dog - structure information about dogs
-*@name: name of dog 
+*@name: name of dog
 *@age: age of dog
 *@owner: owner of dog
 */
 
-typedef struct dog 
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 
 
 int _putchar(char);
@@ -22,4 +22,4 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 
-#endif 
+#endif
