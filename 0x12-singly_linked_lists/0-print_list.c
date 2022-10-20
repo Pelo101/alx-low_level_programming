cc1e6f1b96a6241c6820f63ee4d8f6e61b9e11b0
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-10-20 16:17:10 -0700

    elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,16 +11,16 @@ size_t print_list(const list_t *h)
 {
 	size_t list = 0;
 
-	while (*h != NULL)
+	while (h != NULL)
 	{
-		if (*h->str == NULL)
-			printf("[%d] %s\n", 0 , "(nil)");
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		list++;
 	}
-	return
+	return (list);
 }
 
 
