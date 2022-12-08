264dec53b5730bc5f8748eb85f5bd0003d99d2d6
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-12-08 05:59:26 -0800

    len

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,44 +1,27 @@
 #include "lists.h"
-
 /**
- * @print_dlistint - Prints all elements of  dlistint_h
- *
- * @h: head of the list
+ *dlistint_len - return all elements of the list
+ *@h : head of the list
  *
  *Return: the number of nodes
- **/
+ */
 
-size_t print_dlistint(const dlistint_t *h)
-
+size_t dlistint_len(const dlistint_t *h)
 {
-
 	int count;
 
 	count = 0;
 
-
 	if (h == NULL)
-
 		return (count);
 
 	while (h->prev != NULL)
-
 		h = h->prev;
 
-
 	while (h != NULL)
-
 	{
-
-		printf("%d\n", h->n);
-
 		count++;
-
 		h = h->next;
-
 	}
-
-
 	return (count);
-
 }
