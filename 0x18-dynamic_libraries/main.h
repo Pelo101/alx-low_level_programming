e551e803fb9a006d929292105f88206f9e0a7d75
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2022-12-11 22:08:45 -0800

    initial commit

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,7 +1,5 @@
-#ifndef _MAIN_H
-#define _MAIN_H
-
-
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 
@@ -42,4 +40,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
-#endif /* _MAIN_H */
+
+#endif /* _MAIN_H_ */
