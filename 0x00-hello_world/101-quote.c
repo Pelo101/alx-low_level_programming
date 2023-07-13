817c6432d4df3013a9520fc14c278db732c3df68
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2023-07-13 04:57:02 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <unistd.h>
 
 /**
- * Main - Entry point
+ * main - Write a C program that prints a sentence followed by newline
  *
  * Return: 1
  */
 
 int main(void)
 {
-	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
 }
