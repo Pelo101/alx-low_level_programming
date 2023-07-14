d3d7ac1b3dac2dc9e88596cb70ac54cd9a309c50
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2023-07-14 03:28:06 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase followed by new line.
+ *
+ * Return: 0 Always
+ */
+
+int main(void)
+
+{       
+	char c;
+
+	for ( c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
