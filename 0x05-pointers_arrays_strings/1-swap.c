56ddfe11ec37c410587d2fa321c541666e7624fa
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2023-07-24 04:01:41 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+* swap_int - function that swaps the values of two integers.
+* @a: parameter.
+* @b: parameter.
+* Return: void
+*/
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
