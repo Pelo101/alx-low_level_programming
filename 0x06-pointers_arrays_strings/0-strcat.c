145118435f64abb53d42f029167f2133ad0579a2
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2023-07-27 10:11:43 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+*_strcat - Write a function that concatenates two strings.
+* @dest: destination string.
+* @src: the string to append.
+* Return: a pointer to the resulting string dest.
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int len, i;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[len] != '\0')
+
+	{
+		len++;
+	}
+
+	dest = '\0';
+
+	return (dest);
+
+}
+
