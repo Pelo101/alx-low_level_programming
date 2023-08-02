29d2d3f5ddad56277c724de314a1d89882002719
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2023-08-02 11:16:15 -0700

    task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,7 +4,8 @@
 /**
 *factorial - function that returns the factorial of a given number.
 *@n: given number.
-*If n is lower than 0, the function should return -1 to indicate an error.
+*Return: n is lower than 0, the function should return -1
+*to indicate an error.
 */
 
 int factorial(int n)
