a153cf3200067f4c2045f84d4c96449bca23aa97
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2023-08-27 03:14:19 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+*_abs - computes absolute value.
+*@n: integer.
+*Return: the absolute value.
+*/
+
+
+int _abs(int n)
+{
+
+	if (n < 0)
+		return (-n);
+
+	return (n);
+}
+
+
+
+/**
 *print_last_digit -  function that prints the last digit of a number.
 *@num: integer.
 *Return: 0
@@ -8,7 +26,7 @@
 int print_last_digit(int num)
 {
 
-	int last_digit = num % 10;
+	int last_digit = _abs(num % 10);
 
 	_putchar('0' + last_digit);
 
