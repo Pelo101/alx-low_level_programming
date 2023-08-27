14f1ad07bd17ccb452bf1cdea3fce7149d3fabb4
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2023-08-27 02:23:04 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,17 +9,17 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 7; i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (j = i + 1; j < 8; j++)
+		for (j = i + 1; j < 9; j++)
 		{
 
-			for (k = j + 1; k < 9; k++)
+			for (k = j + 1; k < 10; k++)
 			{
 				putchar('0' + i);
 				putchar('0' + j);
 				putchar('0' + k);
-				if (i != 7 || j != 8 || k != 9)
+				if (i != 8 || j != 9 || k != 10)
 				{
 					putchar(',');
 					putchar(' ');
