335f9b38e498230a9e40b1683376ae05e18a9177
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2023-08-27 10:39:54 -0700

    update

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,20 +1,5 @@
 #include <stddef.h>
-
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-
-/**
- * struct op - Struct op
- * @op: The operator
- * @f: The function associated
- */
-typedef struct op {
-    char *op;
-    int (*f)(int, int);
-} op_t;
+#include "3-calc.h"
 
 
 /**
