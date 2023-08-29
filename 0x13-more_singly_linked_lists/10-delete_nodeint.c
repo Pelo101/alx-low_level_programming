572a9a67d6aafee3db29b44c2d538d57caacde6c
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2023-08-29 02:49:36 -0700

    task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,44 +8,37 @@
 *@index: index of node.
 *Return: 1 if it succeeded -1 if it fails.
 */
+
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-
 	listint_t *current;
-	listint_t *prev, *temp;
+	listint_t *previous;
 	unsigned int i;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (-1);
 
-	if (index == 0)
-	{
-		temp  = *head;
-		*head = (*head)->next;
-		free(temp);
-		return (1);
-
-	}
 	current = *head;
-	prev = NULL;
+	previous = NULL;
+
 
 	for (i = 0; current != NULL && i < index; i++)
-
 	{
-		prev = current;
+		previous = current;
 		current = current->next;
-
 	}
 
 	if (current == NULL)
 		return (-1);
 
-	if (prev != NULL)
-	prev->next = current->next;
-
+	if (previous != NULL)
+		previous->next = current->next;
 	else
-	*head = current->next;
+		*head = current->next;
 
 	free(current);
 	return (1);
 }
+
+
+
