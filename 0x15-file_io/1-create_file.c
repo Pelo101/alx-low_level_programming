cbcc59aa4dd6680f78d28642cf261d4ed291c893
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2023-09-04 11:37:02 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,6 +20,13 @@ int create_file(const char *filename, char *text_content)
 	{
 
 		return (-1);
+
+	}
+	if (text_content != NULL)
+	{
+		while (text_content[len] != '\0')
+			len++;
+
 	}
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
