cbb5168d6fc3301a82c8b3fc0c7030b1a599a79a
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2023-09-04 10:39:09 -0700

    header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H_
 #define MAIN_H_
-
+#include <stddef.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
@@ -8,4 +11,4 @@ int append_text_to_file(const char *filename, char *text_content);
 
 
 
-#endif
+#endif /* MAIN_H_ */
