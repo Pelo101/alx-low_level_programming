848d8a6869d0bdf109829fd916f9b262303cfebb
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2023-11-27 09:28:37 -0800

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,48 @@
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * create_file - function that creates a file.
+ *@filename: file to be created.
+ *@text_content: text string to be inputed into file.
+ *Return: 1 on Success
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+
+	int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC ,0600);
+
+	int len = strlen(text_content);
+
+	ssize_t b;
+
+
+	if (fd == -1)
+	{
+		return (0);
+
+	}
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		b = write(fd, text_content, len);
+
+		if (b == -1)
+			return (-1);
+	}
+
+	close(fd);
+	return (1);
+
+}
