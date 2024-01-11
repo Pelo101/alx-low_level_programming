50d21451b9382dec8bdc8118213e4e22c8cc902e
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-01-11 04:07:15 -0800

    task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *print_dlistint - Write a function that prints all the elements of a dlistint_t list.
+ *@h: head of list.
+ *Return: linked list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
