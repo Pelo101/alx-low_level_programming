ec3d9ada81ace403260a8738a26b14185a9bc79c
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-01-15 02:48:21 -0800

    header

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,6 @@
-#define MAIN_H_
-#ifndef MAIN_H_
 
+#ifndef MAIN_H_
+#define MAIN_H_
 
 int _putchar(char c);
 int _islower(int c);
@@ -23,4 +23,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* MAIN_H_ */
